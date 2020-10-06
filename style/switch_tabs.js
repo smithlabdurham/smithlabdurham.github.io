1102374e782d4bdaefa3cb8ae02dfbcf900de200
Martin Smith <martins@gmail.com>
2020-10-06 15:48:25 +0100

    Software

--- style/switch_tabs.js
+++ style/switch_tabs.js
@@ -2,16 +2,19 @@ $(document).ready(function() {
   $('#biography_content').css('display', 'none');
   $('#fieldwork_content').css('display', 'none');
   $('#teaching_content').css('display', 'none');
+  $('#software_content').css('display', 'none');
 
   function unselect_tabs() {
     $('#research_content').css('display', 'none');
     $('#biography_content').css('display', 'none');
     $('#fieldwork_content').css('display', 'none');
     $('#teaching_content').css('display', 'none');
+    $('#software_content').css('display', 'none');
     $('#research_tab').addClass('other_tab');
     $('#biography_tab').addClass('other_tab');
     $('#fieldwork_tab').addClass('other_tab');
     $('#teaching_tab').addClass('other_tab');
+    $('#software_tab').addClass('other_tab');
   }
   $('#research_tab').click(function() {
     unselect_tabs();
@@ -32,5 +35,10 @@ $(document).ready(function() {
     unselect_tabs();
     $('#biography_tab').removeClass('other_tab');
     $('#biography_content').css('display', 'block');
+  });
+  $('#software_tab').click(function() {
+    unselect_tabs();
+    $('#software_tab').removeClass('other_tab');
+    $('#software_content').css('display', 'block');
   });
 });
