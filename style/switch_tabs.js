6dda04084ad2adb30530f8e37e0025c8a9c12539
Martin Smith <martins@gmail.com>
2020-10-07 09:25:05 +0100

    Separate recruitment tabs

--- style/switch_tabs.js
+++ style/switch_tabs.js
@@ -1,30 +1,46 @@
 $(document).ready(function() {
   $('#biography_content').css('display', 'none');
-  $('#fieldwork_content').css('display', 'none');
+  $('#phd_content').css('display', 'none');
+  $('#msc_content').css('display', 'none');
+  $('#opps_content').css('display', 'none');
   $('#teaching_content').css('display', 'none');
   $('#software_content').css('display', 'none');
 
   function unselect_tabs() {
     $('#research_content').css('display', 'none');
     $('#biography_content').css('display', 'none');
-    $('#fieldwork_content').css('display', 'none');
+    $('#phd_content').css('display', 'none');
+    $('#msc_content').css('display', 'none');
+    $('#opps_content').css('display', 'none');
     $('#teaching_content').css('display', 'none');
     $('#software_content').css('display', 'none');
     $('#research_tab').addClass('other_tab');
     $('#biography_tab').addClass('other_tab');
-    $('#fieldwork_tab').addClass('other_tab');
     $('#teaching_tab').addClass('other_tab');
     $('#software_tab').addClass('other_tab');
+    $('#phd_tab').addClass('other_tab');
+    $('#msc_tab').addClass('other_tab');
+    $('#opps_tab').addClass('other_tab');
   }
   $('#research_tab').click(function() {
     unselect_tabs();
     $('#research_content').css('display', 'block');
     $('#research_tab').removeClass('other_tab');
   });
-  $('#fieldwork_tab').click(function() {
+  $('#phd_tab').click(function() {
     unselect_tabs();
-    $('#fieldwork_tab').removeClass('other_tab');
-    $('#fieldwork_content').css('display', 'block');
+    $('#phd_tab').removeClass('other_tab');
+    $('#phd_content').css('display', 'block');
+  });
+  $('#msc_tab').click(function() {
+    unselect_tabs();
+    $('#msc_tab').removeClass('other_tab');
+    $('#msc_content').css('display', 'block');
+  });
+  $('#opps_tab').click(function() {
+    unselect_tabs();
+    $('#opps_tab').removeClass('other_tab');
+    $('#opps_content').css('display', 'block');
   });
   $('#teaching_tab').click(function() {
     unselect_tabs();
