3047bd4d1b1d648028cae41f7cc686fcd0b90d9d
Martin Smith <martins@gmail.com>
2020-10-13 13:02:33 +0100

    Hash URLs

--- style/switch_tabs.js
+++ style/switch_tabs.js
@@ -6,55 +6,37 @@ $(document).ready(function() {
   $('#teaching_content').css('display', 'none');
   $('#software_content').css('display', 'none');
 
-  function unselect_tabs() {
-    $('#research_content').css('display', 'none');
-    $('#biography_content').css('display', 'none');
-    $('#phd_content').css('display', 'none');
-    $('#msc_content').css('display', 'none');
-    $('#opps_content').css('display', 'none');
-    $('#teaching_content').css('display', 'none');
-    $('#software_content').css('display', 'none');
-    $('#research_tab').addClass('other_tab');
-    $('#biography_tab').addClass('other_tab');
-    $('#teaching_tab').addClass('other_tab');
-    $('#software_tab').addClass('other_tab');
-    $('#phd_tab').addClass('other_tab');
-    $('#msc_tab').addClass('other_tab');
-    $('#opps_tab').addClass('other_tab');
+  function select_tab(id) {
+    unselect_tabs();
+    $('#' + id + '_content').css('display', 'block');
+    $('#' + id + '_tab').removeClass('other_tab');
+    location.hash = "#!" + id;
   }
-  $('#research_tab').click(function() {
-    unselect_tabs();
-    $('#research_content').css('display', 'block');
-    $('#research_tab').removeClass('other_tab');
-  });
-  $('#phd_tab').click(function() {
-    unselect_tabs();
-    $('#phd_tab').removeClass('other_tab');
-    $('#phd_content').css('display', 'block');
-  });
-  $('#msc_tab').click(function() {
-    unselect_tabs();
-    $('#msc_tab').removeClass('other_tab');
-    $('#msc_content').css('display', 'block');
-  });
-  $('#opps_tab').click(function() {
-    unselect_tabs();
-    $('#opps_tab').removeClass('other_tab');
-    $('#opps_content').css('display', 'block');
-  });
-  $('#teaching_tab').click(function() {
-    unselect_tabs();
-    $('#teaching_tab').removeClass('other_tab');
-    $('#teaching_content').css('display', 'block');
-  });
-  $('#biography_tab').click(function() {
-    unselect_tabs();
-    $('#biography_tab').removeClass('other_tab');
-    $('#biography_content').css('display', 'block');
-  });
-  $('#software_tab').click(function() {
-    unselect_tabs();
-    $('#software_tab').removeClass('other_tab');
-    $('#software_content').css('display', 'block');
-  });
+
+  function unselect_tab(id) {
+    $('#' + id + '_content').css('display', 'none');
+    $('#' + id + '_tab').addClass('other_tab');
+  }
+
+  function unselect_tabs() {
+    unselect_tab('research');
+    unselect_tab('team');
+    unselect_tab('masters');
+    unselect_tab('phd');
+    unselect_tab('opportunities');
+    unselect_tab('software');
+  }
+
+  $('#research_tab').click(function() { select_tab('research')});
+  $('#team_tab').click(function() {select_tab('team')});
+  $('#masters_tab').click(function() {select_tab('masters')});
+  $('#phd_tab').click(function() {select_tab('phd')});
+  $('#opportunities_tab').click(function() {select_tab('opportunities')});
+  $('#teaching_tab').click(function() {select_tab('teaching')});
+  $('#software_tab').click(function() {select_tab('software')});
+
+  if (location.hash) {
+    select_tab(location.hash.replace(/#!?/, ""));
+  }
+
 });
